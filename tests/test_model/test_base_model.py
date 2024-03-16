cf3a08923a24584ff8b678508a2f47ae42fc4950
kcsandyy <kemboisandra20@gmail.com>
2024-03-16 05:17:50 -0700

    Resolved unitest syntax error

--- tests/test_model/test_base_model.py
+++ tests/test_model/test_base_model.py
@@ -6,7 +6,7 @@ Test module
 import unittest
 from models.base_model import BaseModel
 
-class TestBasemodel(unnittest.TestCase):
+class TestBasemodel(unittest.TestCase):
     def test_init(self):
         my_model = BaseModel()
 
@@ -18,26 +18,27 @@ class TestBasemodel(unnittest.TestCase):
         my_model = BaseModel()
 
         initial_updated_at = my_model.updated_at
-        current_updated_at = my_model.save()
+        my_model.save()
+        current_updated_at = my_model.updated_at
 
-        self.assertNotEqual(initial_updated_at. current_updated_at)
+        self.assertNotEqual(initial_updated_at,  current_updated_at)
 
     def test_to_dict(self):
         my_model = BaseModel()
         
         my_model_dict = my_model.to_dict()
-        self.assertIsInstance(my_model_dict. dict)
-        self.assertEqual(my_model_dict["__class__"]. 'BaseModel')
-        self.assertEqual(my_model_dict['id']. my_model.id)
-        self.assertEqual(my_model_dict['created_at']. my_model.created_at.isoformat())
-        self.assertEqual(my_model_dict["updated_at"]. my_model.updated_at.isoformat())
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertEqual(my_model_dict["__class__"], "BaseModel")
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict["updated_at"], my_model.updated_at.isoformat())
 
     def test_str(self):
         my_model = BaseModel()
 
         self.assertTrue(str(my_model).startswith('[BaseModel]'))
-        self.assertIn(my_model.id. str(my_model))
-        self.assertIn(str(my_model.__dict__). str(my_model))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
 
 if __name__ == "__main__":
     unittest.main()
