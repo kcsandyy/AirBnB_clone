02fcd24b85ef1c08a4c29269a175c636b4c8af65
kcsandyy <kemboisandra20@gmail.com>
2024-03-28 12:57:56 -0700

    test shells

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,62 @@
 #!/usr/bin/python3
 """
-A class that defines all common attribute
-for other classes
+Module: base.py
 """
+import models
 import uuid
 from datetime import datetime
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
 
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+class BaseModel():
+    """
+    Base class which defines all common
+    attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        instatiates an object with it's
+        attributes
+        """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        models.storage.new(self)
+
+    def __str__(self):
+        """
+        Returns the string representation
+        of the instance
+        """
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
-        updates the public instance attribute updated_at
-
+        updates the public instance attribute
+        updated_at with the current datetime
         """
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
-        returns a dictionary
-
+        returns a dictionary containing all keys/values
+        of __dict__ of the instance
         """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
+        dict = {**self.__dict__}
+        dict['__class__'] = type(self).__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
 
-        return inst_dict
-    
-    def __str__(self):
-        """
-        print: [<class name>] (<self.id>) <self.__dict__>
-
-        """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        return dict
